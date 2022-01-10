6a0284a5a7f5b49df3031aae65c1b247a069f4db
Grace Ramey <graceramey@pcd-ak2q05d-lt.ucsf.edu>
2022-01-10 13:51:43 -0800

    Updated comments

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -6,7 +6,8 @@ def transcribe(seq: str) -> str:
     transcribes DNA to RNA by replacing
     all `T` to `U`
     """
-    transcript = seq.replace("T", "U") #.replace() function iterates through seq & replaces "T" with "U" (effectively transcribing the DNA sequence)
+    #.replace() function iterates through letters of seq & replaces "T" with "U" (effectively transcribing the DNA sequence)
+    transcript = seq.replace("T", "U") 
     
     return transcript
 
@@ -17,6 +18,6 @@ def reverse_transcribe(seq: str) -> str:
     all `T` to `U` then reverses the sequence
     """
     transcript = transcribe(seq) #call transcribe function to transcribe the sequence (convert Ts to Us)
-    revTranscript = transcript[::-1] #reverse the transcribed string using the reverse function
+    revTranscript = transcript[::-1] #then reverse the transcribed string using this notation
     
     return revTranscript
